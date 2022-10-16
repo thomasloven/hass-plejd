de279f2101be80607a98c46120838d3c63def15d
Øyvind Matheson Wergeland <oyvind@wergeland.org>
2022-10-16 21:44:14 +0200

    Support macOS address format.

--- custom_components/plejd/pyplejd/mesh.py
+++ custom_components/plejd/pyplejd/mesh.py
@@ -74,7 +74,7 @@ class PlejdMesh():
             return False
 
         self.client = client
-        self.connected_node = binascii.a2b_hex(address.replace(":", ""))[::-1]
+        self.connected_node = binascii.a2b_hex(address.replace(":", "").replace("-", ""))[::-1]
 
         await asyncio.sleep(2)
 
