548e2b011649071e61f115f8347294812b9bc9c6
Thomas Lovén <thomasloven@gmail.com>
2023-08-26 23:38:42 +0200

    Fix diagnostic info

--- custom_components/plejd/diagnostics.py
+++ custom_components/plejd/diagnostics.py
@@ -1,7 +1,11 @@
 import pyplejd
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
 
-async def async_get_config_entry_diagnostics(hass, config_entry):
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, config_entry: ConfigEntry
+):
+    """Return the plejd site configureation from the cloud"""
     plejdManager = pyplejd.PlejdManager(config_entry.data)
-
-    return await plejdManager.get_site_data()
+    return await plejdManager.get_raw_sitedata()
