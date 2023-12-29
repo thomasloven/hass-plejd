585d764239f93b8739f23d2455865e73e014d662
Thomas Lovén <thomasloven@gmail.com>
2023-12-30 00:10:15 +0100

    Reauthentication config flow

--- custom_components/plejd/config_flow.py
+++ custom_components/plejd/config_flow.py
@@ -1,6 +1,6 @@
 import voluptuous as vol
 import logging
-from homeassistant.config_entries import ConfigFlow
+from homeassistant.config_entries import ConfigFlow, ConfigEntry
 from homeassistant.components import bluetooth
 
 from pyplejd import get_sites
@@ -12,15 +12,29 @@ _LOGGER = logging.getLogger(__name__)
 class PlejdConfigFlow(ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
-    def __init__(self):
-        self._discovered = False
+    reauth_entry: ConfigEntry|None = None
 
     async def async_step_bluetooth(self, discovery_info):
-        self._discovered = True
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
         if self._async_in_progress():
             return self.async_abort(reason="single_instance_allowed")
+        return await self.async_step_user()
+
+    async def async_step_reauth(self, user_input=None):
+        """Perform reauth if login failed."""
+        self.reauth_entry = self.hass.config_entries.async_get_entry(
+            self.context["entry_id"]
+        )
+        return await self.async_step_reauth_confirm()
+
+    async def async_step_reauth_confirm(self, user_input=None):
+        """Dialog to inform user that reauthorization is required."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="reauth_confirm",
+                data_schema=vol.Schema({}),
+            )
         return await self.async_step_user()
 
     async def async_step_user(self, info=None):
@@ -37,27 +51,46 @@ class PlejdConfigFlow(ConfigFlow, domain=DOMAIN):
         return await self.async_step_picksite()
 
     async def async_step_picksite(self, info=None):
+        if self.reauth_entry:
+            self.hass.config_entries.async_update_entry(
+                self.reauth_entry,
+                title = self.reauth_entry.data["siteTitle"],
+                data = {
+                    "username": self.credentials["username"],
+                    "password": self.credentials["password"],
+                    "siteId": self.reauth_entry.data["siteId"],
+                    "siteTitle": self.reauth_entry.data["siteTitle"],
+                }
+                )
+            await self.hass.config_entries.async_reload(self.reauth_entry.entry_id)
+            return self.async_abort(reason="reauth_successful")
+
         if info is None:
             sites = await get_sites(
                 self.credentials["username"], self.credentials["password"]
             )
             self.sites = {
+                site.siteId: site.title for site in sites
+            }
+            options = {
                 site.siteId: f"{site.title} ({site.deviceCount} devices)"
                 for site in sites
             }
             return self.async_show_form(
                 step_id="picksite",
-                data_schema=vol.Schema({vol.Required("site"): vol.In(self.sites)}),
+                data_schema=vol.Schema({vol.Required("site"): vol.In(options)}),
             )
 
         await self.async_set_unique_id(info["site"])
         self._abort_if_unique_id_configured()
 
-        siteTitle = self.sites[info["site"]]
+        siteId = info["site"]
+        siteTitle = self.sites[siteId]
+
         data = {
             "username": self.credentials["username"],
             "password": self.credentials["password"],
-            "siteId": info["site"],
+            "siteId": siteId,
             "siteTitle": siteTitle,
         }
         return self.async_create_entry(title=siteTitle, data=data)
