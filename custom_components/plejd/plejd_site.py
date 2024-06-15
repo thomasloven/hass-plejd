05a5e33b0072efb7709641f6fb653b1ca81c89de
Pierre Ingmansson <pierrei@spotify.com>
2024-06-15 21:12:03 +0200

    Only ping if the device discovered is new

--- custom_components/plejd/plejd_site.py
+++ custom_components/plejd/plejd_site.py
@@ -154,8 +154,8 @@ class PlejdSite:
 
     def _discovered(self, service_info: BluetoothServiceInfoBleak, *_, connect: bool = True) -> None:
         """Register any discovered plejd device with the manager."""
-        self.manager.add_mesh_device(service_info.device, service_info.rssi)
-        if connect:
+        new_device = self.manager.add_mesh_device(service_info.device, service_info.rssi)
+        if connect and new_device:
             self.hass.async_create_task(self._ping())
 
     async def _ping(self, *_) -> None:
